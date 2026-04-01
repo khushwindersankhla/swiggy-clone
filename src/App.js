eaadb0add0be63bf0a618ddabe176a7b4136017f
anilghaswan <ghaswananil56@gmail.com>
2026-04-01 13:11:05 +0530

    uploded delivery page and add footer commpontes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // import Header from './Componets/Header/Header.jsx';
 // import Mainpage from './Componets/Mainpage/Mainpage.jsx';
-// import Footer from './Componets/Footer/Footer.jsx';
+// import Footer from "./Componets/Footer/Footer.jsx";
 // import Navbar from './Componets/Navbar/Navbar.jsx';
-   import AboutSwiggy from './Page/AboutSwigg/AboutSwiggy.jsx'
+// import AboutSwiggy from "./Page/AboutSwigg/AboutSwiggy.jsx";
+// import Delivery from "./Page/AboutSwigg/Delivery/Delivery.jsx";
 
 function App() {
   return (
     <>
-    {/* <Header/>
+      {/* <Header/>
     <Mainpage/>
     <Navbar/>
-    <Footer/> */}
-    <AboutSwiggy/>
+   */}
+      {/* <AboutSwiggy/> */}
+      {/* <Delivery /> */}
+      {/* <Footer /> */} */
     </>
   );
 }
